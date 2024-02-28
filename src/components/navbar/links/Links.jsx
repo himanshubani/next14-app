7a2ff3a0d84dba9f47bc2987c4bfbd13fe80c591
himanshu <himanshu.bani123@gmail.com>
2024-02-28 20:04:48 +0530

    Add responsive container and styling to navbar and links

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,31 +1,68 @@
-import Link from "next/link";
+"use client";
+
+import styles from "./links.module.css";
+import NavLink from "./navLink/NavLink";
+import { useState } from "react";
+
+const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 
 const Links = () => {
-  const links = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div>
-      {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button className={styles.menuButton} onClick={() => setOpen(!open)}>
+        Menu
+      </button>
+      {
+        open && (
+          <div className={styles.mobileLinks}>
+            {links.map((link) => (
+              <NavLink item={link} key={link.title} />
+            ))}
+            {session ? (
+              <>
+                {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+                <button className={styles.logout}>Logout</button>
+              </>
+            ) : (
+              <NavLink item={{ title: "Login", path: "/login" }} />
+            )}
+          </div>
+        )
+      }
     </div>
   );
 };
